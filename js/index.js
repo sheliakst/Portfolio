214e42870421e26e034d769fe02d82dc838df09c
sheliakst <stamcj@hotmail.com>
2018-11-21 13:20:07 +0000

    add skillShow on click

--- js/index.js
+++ js/index.js
@@ -32,18 +32,56 @@ $(window).on('mousemove click', function (e) {
 });
 
 moveBackground();
-
 $(document).ready(function () {
   AOS.init();
 
-$('.skillanimationLogos').on("click",function(){
 
-  $("#path815").animate({"stroke-dashoffset": "400"},1000
-  );
-  $("#path815").animate({"stroke-dashoffset": "140"},1111
-);
 
-});
+  /*  var animationSkills = true;
+ 
+   $('.skillanimationLogos').on("click", function () {
+    
+    if (animationSkills == true){
+     animationSkills = false;
+     $("#path815").animate({ "stroke-dashoffset": "400" }, 1000
+     );
+     $("#path815").animate({ "stroke-dashoffset": "140" }, 1111
+     );
+    }
+    animationSkills = true;
+ 
+   });
+ 
+ 
+  */
+
+
+
+
+
+  $(".skillsvg").on("click", function () {
+      var skillShow= $(this).find('#path815');
+
+    $(skillShow).animate({ "stroke-dashoffset": "400" }, 1000
+    );
+    $(skillShow).animate({ "stroke-dashoffset": "140" }, 1111
+    );
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -53,15 +91,15 @@ $('.skillanimationLogos').on("click",function(){
 
     /* parallax */
     $('.backgroundImage2 ').css('top', (scrollh * 1.201) + 'px');
-  
+
     /*   scroll navbar */
-   
+
     if (scrollh > navheight) {
-   
-       $('.navbar').css('background-image', ' linear-gradient(rgba(119, 119, 119, 1),rgba(119, 119, 119, 0.9),rgba(119, 119, 119, 0.8) ,rgba(119, 119, 119, 0))') ;
+
+      $('.navbar').css('background-image', ' linear-gradient(rgba(119, 119, 119, 1),rgba(119, 119, 119, 0.9),rgba(119, 119, 119, 0.8) ,rgba(119, 119, 119, 0))');
 
 
-      } else {
+    } else {
       $('.navbar').css('background', 'rgba(119, 119, 119, 0)');
 
     }
